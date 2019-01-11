1ba2f15d1b42cfd373ca873955c00d86419bffa1
Cabelytos <39059990+Cabelytos@users.noreply.github.com>
2019-01-11 13:42:21 -0200

    Xadrez
    Construindo um jogo de xadrez para treino a programação orientada a objeto!

---
+++ Xadrez_Console/Xadrez_Console/xadrez/Cavalo.cs
@@ -0,0 +1,125 @@
+﻿using tabuleiro;
+
+namespace xadrez
+{
+    class Cavalo : Peca
+    {
+
+        public Cavalo(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "C";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            //acima direita
+            pos.definirValores(posicao.linha - 2, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 8;
+            }
+
+            //pra cima esquerda
+            pos.definirValores(posicao.linha - 2, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.coluna = pos.coluna - 8;
+            }
+
+            //abaixo esquerda
+            pos.definirValores(posicao.linha + 2, posicao.coluna - 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 8;
+            }
+
+            //abaixo direita
+            pos.definirValores(posicao.linha + 2, posicao.coluna + 1);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 8;
+            }
+
+            // direita pra baixo 
+            pos.definirValores(posicao.linha + 1, posicao.coluna + 2);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha - 8;
+            }
+            // esqurda pra baixo 
+            pos.definirValores(posicao.linha - 1, posicao.coluna - 2);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 8;
+            }
+
+            // direita pra cima 
+            pos.definirValores(posicao.linha - 1, posicao.coluna + 2);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 8;
+            }
+            // esquerda pra cima 
+            pos.definirValores(posicao.linha + 1, posicao.coluna - 2);
+            while (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.linha, pos.coluna] = true;
+                if (tab.peca(pos) != null && tab.peca(pos).cor != cor)
+                {
+                    break;
+                }
+                pos.linha = pos.linha + 8;
+            }
+            return mat;
+        }
+
+    }
+}
